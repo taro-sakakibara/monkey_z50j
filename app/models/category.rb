10390d57906c01d10c7d525acd9e53b93f966574
taro-sakakibara <theband0914@gmail.com>
2020-12-08 09:42:32 +0900

    アクティブハッシュの実装

---
+++ app/models/category.rb
@@ -0,0 +1,12 @@
+class Category < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '1' },
+    { id: 3, name: '2' },
+    { id: 4, name: '3' },
+    { id: 5, name: '4' },
+    { id: 6, name: '5' },
+    { id: 7, name: '6' },
+  ]
+  has_many :tweets
+  end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -2,11 +2,12 @@ class Tweet < ApplicationRecord
 
   with_options presence: true do
     validates :text,length:{maximum:140}
-    validates :category_id
+    validates :category_id, numericality: { other_than: 1 }
     validates :image
   end
 
  belongs_to :user
+ belongs_to :category
  has_many :comments, dependent: :destroy
  has_one_attached :image
 end
