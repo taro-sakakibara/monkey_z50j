db63707c70bba2a01efa2d5d3cac95084c4abd0c
taro-sakakibara <theband0914@gmail.com>
2020-12-13 16:04:50 +0900

    time_zoneの変更

--- app/models/category.rb
+++ app/models/category.rb
@@ -3,7 +3,7 @@ class Category < ActiveHash::Base
     { id: 1, name: '--' },
     { id: 2, name: '今日の散歩' },
     { id: 3, name: '今日のコーヒー' },
-    { id: 4, name: '今日のサラダ' },
+    { id: 4, name: '今日のランチ' },
     { id: 5, name: '今日の休憩' },
     { id: 6, name: '今日の成果' },
     { id: 7, name: '今日のお風呂' },
